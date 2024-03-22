e6d82c8d4a1f9ce2bdd0ac4a9da7b34d6d21b679
UjwlJain <ujjawalpugalia09@gmail.com>
2024-03-22 15:47:32 +0530

    updated changes

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -5,10 +5,9 @@ const NewsBoard = ({category}) => {
 
   const[articles,setArticles]= useState([]);
   useEffect(() => {
-    let url = `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=24d596393c824f5db0ad4ede472579da`;
+    let url = `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=${import.meta.env.VITE_NEWS_API_KEY}`;
     fetch(url).then(response=> response.json()).then(data=> setArticles(data.articles));
   }, [category])
-  
   return (
     <div>
         <h2 className='text-center text-danger'>Latest <span> News</span></h2>
